753a7c59b8f1a8ff44f09e771e880ae8eb1cb52f
asraavisena <asra.avisena@yahoo.com>
2021-01-03 10:49:05 +0100

    adding authentication

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/react', function () {
     return view('react-welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
