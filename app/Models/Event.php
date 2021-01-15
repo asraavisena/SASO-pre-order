bbfb7beeea22da9c0cf509ff5156435a88aca1fb
asraavisena <asra.avisena@yahoo.com>
2021-01-15 15:08:52 +0100

    change the path of the image in models

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -9,9 +9,11 @@ class Event extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'event_image', 'desc', 'started_at'];
+    public $path = "/storage/";
 
     // Accessor; returning something
     public function getEventImageAttribute($value){
-        return asset($value);
+        // return asset($value);
+        return $this->path . $value;
     }
 }
--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -9,10 +9,13 @@ class Menu extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'menu_image', 'desc', 'quantity', 'price'];
+    public $path = "/storage/";
 
     // Accessor; returning something
     public function getMenuImageAttribute($value){
-        return asset($value);
+        // return asset($value);
+        return $this->path . $value;
+
     }
 
     public function category(){
