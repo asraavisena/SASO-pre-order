34bb25bc24d0bae249ecfdd846f229d777b2efb8
ilhamuh97 <ilhamuh97@gmail.com>
2021-01-23 15:00:05 +0100

    fix and improve design

--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -7,7 +7,11 @@ const Header = () => {
         <div className="header">
             <div className="header-wrapper ">
                 <div className="img-holder">
-                    <a href="https://iwkz.de/">
+                    <a
+                        href="https://iwkz.de/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
                         <img src="https://iwkz.de/static/media/iwkz-navbar.80c680cd.svg"></img>
                     </a>
                 </div>
