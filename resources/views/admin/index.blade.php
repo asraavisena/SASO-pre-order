8259cb8589edcea51d28c64bfd248702c651e724
asraavisena <asra.avisena@yahoo.com>
2021-04-28 11:10:40 +0700

    adding user name in dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,6 +1,9 @@
 <x-admin-master>
     @section('content')
-    <h1 class="h1 mb-4 text-gray-800">Dashboard</h1>
-    <h2 class= "h2 mb-2">Welcome</h2>
+    <h1 class="h2 mb-4 text-gray-800">Dashboard</h1>
+    <h2 class= "h4 mb-2">Welcome 
+        @if(Auth::check())
+            {{auth()->user()->name}}
+        @endif</h2>
     @endsection
 </x-admin-master>
